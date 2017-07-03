f74fe5a87691dca8bf19e15b09930a1f13cc47ac
jhyun94 <hyunjonathan@gmail.com>
2017-07-03 15:26:26 -0500

    configured for heroku

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var path = require('path');
 
 var app = express();
-
+var port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -11,6 +11,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log('server is running on port 3000');
 })
