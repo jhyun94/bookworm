97bba3b299cf6cbdcf228fafe799d31ac25e89ae
jhyun94 <hyunjonathan@gmail.com>
2017-07-17 14:45:48 -0500

    able to signup and login then logout successfully

--- server/public/js/index.js
+++ server/public/js/index.js
@@ -1,14 +1,22 @@
 //event for signup
-$('#main-form').on('submit', function(e){
+$('#main-form button').on('click', function(e){
   e.preventDefault();
   var formData = {
     email: $('input[name=email]').val(),
     password: $('input[name=password]').val()
   }
+  if (this.id === 'signup') {
+    signup(formData);
+  }else if(this.id === 'login') {
+    login(formData);
+  }
+})
+
+function signup(data){
   $.ajax({
     url: './users',
     method: 'POST',
-    data: formData,
+    data: data,
     success: function(response, status, xhr){
       localStorage.setItem('x-auth', xhr.getResponseHeader('x-auth'));
       localStorage.setItem('x-id', response._id);
@@ -18,5 +26,20 @@ $('#main-form').on('submit', function(e){
       alert('err');
     }
   })
-})
+}
 
+function login(data) {
+  $.ajax({
+    url: './users/login',
+    method: 'POST',
+    data: data,
+    success: function(response, status, xhr){
+      localStorage.setItem('x-auth', xhr.getResponseHeader('x-auth'));
+      localStorage.setItem('x-id', response._id);
+      window.location.href = './book.html'
+    },
+    error: function(){
+      alert('err');
+    }
+  })
+}
